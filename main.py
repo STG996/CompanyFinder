75d4081ca6c1f44a4760197ec2c69ac061163825
STG996 <sami.zayed8@gmail.com>
2024-11-22 15:53:01 +0200

    Add brief license text to main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+'''
+CompanyFinder: AQA non-exam assessment submission 2025
+Copyright (C) 2024  STG996
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see https://www.gnu.org/licenses/.
+'''
+
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
