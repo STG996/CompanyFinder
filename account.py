ccefd4f50dd04640057d187b526f9271fbba2ad5
STG996 <sami.zayed8@gmail.com>
2024-12-28 17:23:06 +0200

    Begin making account details encryption on client end

--- account.py
+++ account.py
@@ -1,40 +1,65 @@
 import os
+import random
 
 USERNAME_INDEX = 1
 EMAIL_INDEX = 2
 PASSWORD_INDEX = 3
 
+ALPHABET = "".join([chr(x) for x in range(ord("A"), ord("Z")+1)])
+
 
 class Account:
     def __init__(self):
-        self.__username = None
-        self.__email = None
-        self.__password = None
-        self.__file_name = "account_info.txt"
-        self.__logged_in = False
+        self._username = None
+        self._email = None
+        self._password = None
+        self._file_name = "account_info.txt"
+        self._logged_in = False
 
     def retrieve_account_from_file(self):
-        file = open(self.__file_name, "r")
+        file = open(self._file_name, "r") # will error if file not found, TODO: fix
         lines = file.readlines()
         file.close()
 
         try:
-            self.__username = lines[USERNAME_INDEX]
-            self.__email = lines[EMAIL_INDEX]
-            self.__password = lines[PASSWORD_INDEX]
-            self.__logged_in = True
+            self._username = lines[USERNAME_INDEX]
+            self._email = lines[EMAIL_INDEX]
+            self._password = lines[PASSWORD_INDEX]
+            self._logged_in = True
         except IndexError:
-            self.__logged_in = False
+            self._logged_in = False
 
     def add_account_to_file(self, username, email, password):
-        file = open(self.__file_name, "w")
+        file = open(self._file_name, "w")
         file.write(f"\n{username}\n{email}\n{password}\n")
         file.close()
         self.retrieve_account_from_file()
 
     def remove_account_from_file(self):
-        os.remove(self.__file_name)
+        os.remove(self._file_name)
 
     # Information hiding
     def get_logged_in(self):
-        return self.__logged_in, self.__username
+        return self._logged_in, self._username
+
+class EncryptedAccount(Account):
+    def __init__(self):
+        super().__init__()
+        self.__key = None
+
+    def __generate_key(self):
+        length_required = len(self._username) + len(self._email) + len(self._password)
+        self.__key = "".join(random.choice(ALPHABET) for x in range(length_required))
+
+    def __encrypt_file(self):
+        file = open(self._file_name, "r")
+        plaintext = file.read()
+        file.close()
+
+        ciphertext = ""
+        for count, character in enumerate(plaintext):
+            ciphertext += chr(ord(character) ^ ord(self.__key[count]))
+
+        file = open(self._file_name, "w")
+        file.write(self.__key + "BREAKER" + ciphertext) # breaker is placeholder TODO: make better breaker
+        file.close()
